af69ddaeccfb2a84a4b116845b2149106c4be09b
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2020-12-01 07:06:59 +0100

    Add Day01 part 1

--- src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day01.java
+++ src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day01.java
@@ -2,21 +2,31 @@ package com.gmail.chickenpowerrr.aoc2020.days;
 
 import com.gmail.chickenpowerrr.aoc2020.framework.Day;
 import com.gmail.chickenpowerrr.aoc2020.helper.file.FileHelper;
-import com.gmail.chickenpowerrr.aoc2020.helper.path.PathHelper;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day01 implements Day {
 
   private final FileHelper fileHelper;
-  private final PathHelper pathHelper;
 
   public Day01() {
     this.fileHelper = new FileHelper();
-    this.pathHelper = new PathHelper();
   }
 
   @Override
   public void partOne() {
+    List<Integer> integers = this.fileHelper.readFileLines("day01/input")
+        .map(Integer::parseInt)
+        .collect(Collectors.toList());
 
+    for (int i = 0; i < integers.size() - 1; i++) {
+      for (int j = i + 1; j < integers.size(); j++) {
+        if (integers.get(i) + integers.get(j) == 2020) {
+          System.out.println(integers.get(i) * integers.get(j));
+          return;
+        }
+      }
+    }
   }
 
   @Override
--- src/main/java/com/gmail/chickenpowerrr/aoc2020/helper/path/PathHelper.java
+++ src/main/java/com/gmail/chickenpowerrr/aoc2020/helper/path/PathHelper.java
@@ -56,6 +56,4 @@ public class PathHelper {
 
     return paths;
   }
-
-
 }
