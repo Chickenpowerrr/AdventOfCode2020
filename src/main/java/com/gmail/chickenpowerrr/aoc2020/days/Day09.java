efe934183cd265a4fa6ad10f474165caffb2cc47
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2020-12-09 07:14:38 +0100

    Add Day09 part 2

--- src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day09.java
+++ src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day09.java
@@ -2,6 +2,7 @@ package com.gmail.chickenpowerrr.aoc2020.days;
 
 import com.gmail.chickenpowerrr.aoc2020.framework.Day;
 import com.gmail.chickenpowerrr.aoc2020.helper.file.FileHelper;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,6 +45,29 @@ public class Day09 implements Day {
     return false;
   }
 
+  private long getEncryptionWeakness(List<Long> session, int preambleLength) {
+    long invalidNumber = getFirstInvalid(session, preambleLength);
+
+    for (int i = 0; i < session.size(); i++) {
+      if (invalidNumber > session.get(i)) {
+        long targetCount = 0;
+        List<Long> range = new ArrayList<>();
+
+        for (int j = i; j < session.size() && targetCount < invalidNumber; j++) {
+          targetCount += session.get(j);
+          range.add(session.get(j));
+        }
+
+        if (targetCount == invalidNumber) {
+          return range.stream().mapToLong(l -> l).min().getAsLong()
+              + range.stream().mapToLong(l -> l).max().getAsLong();
+        }
+      }
+    }
+
+    return -1;
+  }
+
   @Override
   public void partOne() {
     System.out.println(getFirstInvalid(this.fileHelper.readFileLines("day09/input")
@@ -52,6 +76,7 @@ public class Day09 implements Day {
 
   @Override
   public void partTwo() {
-
+    System.out.println(getEncryptionWeakness(this.fileHelper.readFileLines("day09/input")
+        .map(Long::parseLong).collect(Collectors.toList()), 25));
   }
 }
