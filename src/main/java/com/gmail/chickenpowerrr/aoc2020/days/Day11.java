6e89762e8f8976b34fda1f4957a8bba3cab2c0c8
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2020-12-11 11:01:19 +0100

    Add Day11 part 2

--- src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day11.java
+++ src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day11.java
@@ -5,6 +5,7 @@ import com.gmail.chickenpowerrr.aoc2020.helper.file.FileHelper;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Day11 implements Day {
@@ -17,7 +18,7 @@ public class Day11 implements Day {
 
   @Override
   public void partOne() {
-    Collection<Seat> seats = getSeats();
+    Collection<Seat> seats = getSeatsWithAdjacent();
 
     while (seats.stream().filter(Seat::applyNext).count() > 0) {
       seats.forEach(Seat::calculateNext);
@@ -28,22 +29,31 @@ public class Day11 implements Day {
 
   @Override
   public void partTwo() {
+    Collection<Seat> seats = getSeatsWithSight();
 
+    while (seats.stream().filter(Seat::applyNext).count() > 0) {
+      seats.forEach(Seat::calculateNext);
+    }
+
+    System.out.println(seats.stream().filter(Seat::isOccupied).count());
   }
 
-  private Collection<Seat> getSeats() {
-    List<List<Seat>> seats = this.fileHelper.readFileLines("day11/input")
+  private List<List<Seat>> getSeats(int threshold) {
+    return this.fileHelper.readFileLines("day11/input")
         .map(line ->
             line.codePoints().mapToObj(c -> (char) c)
                 .map(character -> {
                   if (character == 'L') {
-                    return new Seat();
+                    return new Seat(threshold);
                   } else {
                     return null;
                   }
                 }).collect(Collectors.toList()))
         .collect(Collectors.toList());
+  }
 
+  private Collection<Seat> getSeatsWithAdjacent() {
+    List<List<Seat>> seats = getSeats(4);
     List<Seat> result = new ArrayList<>();
 
     for (int i = 0; i < seats.size(); i++) {
@@ -74,13 +84,60 @@ public class Day11 implements Day {
     return result;
   }
 
+  private Collection<Seat> getSeatsWithSight() {
+    List<List<Seat>> seats = getSeats(5);
+    List<Seat> result = new ArrayList<>();
+
+    for (int i = 0; i < seats.size(); i++) {
+      for (int j = 0; j < seats.get(i).size(); j++) {
+        Seat target = seats.get(i).get(j);
+
+        if (target != null) {
+          result.add(target);
+
+          for (int[] delta : Set.of(new int[] {0, 1}, new int[] {0, -1}, new int[] {1, 0},
+              new int[] {-1, 0}, new int[] {1, 1}, new int[] {-1, -1}, new int[] {1, -1},
+              new int[] {-1, 1})) {
+
+            Seat cursor = findFirstInSight(seats, i, j, delta);
+            if (cursor != null) {
+              target.addAdjacent(cursor);
+            }
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+
+  private Seat findFirstInSight(List<List<Seat>> seats, int x, int y, int[] delta) {
+    int i = x + delta[0];
+    int j = y + delta[1];
+
+    while (i < seats.size() && i >= 0 && j < seats.get(i).size() && j >= 0) {
+      Seat cursor = seats.get(i).get(j);
+      if (cursor != null) {
+        return cursor;
+      }
+
+      i += delta[0];
+      j += delta[1];
+    }
+
+    return null;
+  }
+
   private static class Seat {
     private final Collection<Seat> adjacent;
+    private final int threshold;
 
     private boolean occupied;
     private boolean nextOccupied;
 
-    public Seat() {
+    public Seat(int threshold) {
+      this.threshold = threshold;
+
       this.occupied = false;
       this.nextOccupied = true;
       this.adjacent = new ArrayList<>();
@@ -92,7 +149,7 @@ public class Day11 implements Day {
 
     public void calculateNext() {
       if (isOccupied()) {
-        this.nextOccupied = this.adjacent.stream().filter(Seat::isOccupied).count() < 4;
+        this.nextOccupied = this.adjacent.stream().filter(Seat::isOccupied).count() < threshold;
       } else {
         this.nextOccupied = this.adjacent.stream().noneMatch(Seat::isOccupied);
       }
@@ -106,10 +163,6 @@ public class Day11 implements Day {
       boolean changed = occupied != nextOccupied;
       this.occupied = nextOccupied;
       return changed;
-    }
-
-    public int getAdjacentCount() {
-      return this.adjacent.size();
     }
   }
 }
