17594172c733265b10828989120432f2647d2f63
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2020-12-22 09:02:57 +0100

    Add Day22 part 2

--- src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day22.java
+++ src/main/java/com/gmail/chickenpowerrr/aoc2020/days/Day22.java
@@ -20,16 +20,85 @@ public class Day22 implements Day {
 
   @Override
   public void partOne() {
-    System.out.println(getWinningScore());
+    System.out.println(calculateWinningScore(simulateGame()));
   }
 
   @Override
   public void partTwo() {
-
+    List<Integer> result = Arrays.stream(simulateRecursiveGame(getDecks()))
+        .filter(list -> !list.isEmpty()).findFirst().get();
+    System.out.println(calculateWinningScore(result));
   }
 
-  private int getWinningScore() {
-    List<Integer> gameResult = simulateGame();
+  @SuppressWarnings("unchecked")
+  private LinkedList<Integer>[] simulateRecursiveGame(LinkedList<Integer>[] startingDecks) {
+    List<LinkedList<Integer>[]> previousOrders = new ArrayList<>();
+
+    LinkedList<Integer>[] decks = Arrays.stream(startingDecks)
+        .map(LinkedList::new)
+        .toArray(LinkedList[]::new);
+
+    while (Arrays.stream(decks).noneMatch(List::isEmpty)) {
+      for (LinkedList<Integer>[] order : previousOrders) {
+        for (int i = 0; i < order.length; i++) {
+          if (order[i].equals(decks[i])) {
+            return decks;
+          }
+        }
+      }
+
+      previousOrders.add(Arrays.stream(decks)
+          .map(LinkedList::new)
+          .toArray(LinkedList[]::new));
+
+      int highestIndex = -1;
+      int highestScore = -1;
+      boolean shouldRecurse = true;
+
+      List<Integer> drawnCards = new ArrayList<>();
+      for (LinkedList<Integer> deck : decks) {
+        int target = deck.pop();
+        drawnCards.add(target);
+
+        if (shouldRecurse) {
+          shouldRecurse = target <= deck.size();
+        }
+
+        if (highestScore < target) {
+          highestScore = target;
+          highestIndex = drawnCards.size() - 1;
+        }
+      }
+
+      if (shouldRecurse) {
+        LinkedList<Integer>[] nextDecks = new LinkedList[decks.length];
+        for (int i = 0; i < decks.length; i++) {
+          nextDecks[i] = new LinkedList<>(decks[i].subList(0, drawnCards.get(i)));
+        }
+
+        LinkedList<Integer>[] recursiveResult = simulateRecursiveGame(nextDecks);
+        for (int i = 0; i < recursiveResult.length; i++) {
+          if (!recursiveResult[i].isEmpty()) {
+            highestIndex = i;
+
+            if (i == 1) {
+              Collections.reverse(drawnCards);
+            }
+
+            break;
+          }
+        }
+      } else {
+        drawnCards.sort(Collections.reverseOrder());
+      }
+
+      decks[highestIndex].addAll(drawnCards);
+    }
+
+    return decks;
+  }
+
+  private int calculateWinningScore(List<Integer> gameResult) {
     return IntStream.range(0, gameResult.size())
         .map(i -> (gameResult.size() - i) * gameResult.get(i)).sum();
   }
